654ea48fb44772a848a2da5394c4fd5526cb612b
Carlos Gonzalez <carlos.gonzalezba@anahuac.mx>
2025-03-03 13:12:06 -0600

    PIT-27: Cambio en la estructura de directorios

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Header.module.css";
+import styles from "../styles/Header.module.css";
 import XuchilLogo from "../../assets/images/Xuchil.svg";
 
 const Header: React.FC = () => {
