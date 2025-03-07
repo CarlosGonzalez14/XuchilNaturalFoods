cba45e504ce86f115fa8111307f8c0bb4f51db4a
Miguel Aragon <mikeqz111@gmail.com>
2025-03-07 09:37:53 -0600

    PIT28-Ejemplo de uso de Componente Card en Sección de Control de Procesos

--- src/pages/ProcessControl.tsx
+++ src/pages/ProcessControl.tsx
@@ -1,8 +1,21 @@
+import Card from "../components/Card";
+
 const ProcessControl = () => {
     return (
       <div className="page">
         <h1>Control de procesos</h1>
         <p>Contenido de la sección Control de procesos.</p>
+
+        <Card>
+        <div>
+          <p><strong>Pedido #<span style={{ fontWeight: "bold" }}>12376</span></strong></p>
+          <p>
+            Blvd. Guadalupe Hinojosa de Murat 1100,<br />
+            71248 San Raymundo Jalpan, Oax.
+          </p>
+        </div>
+        </Card>
+
       </div>
     );
   };
